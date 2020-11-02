1c121014ca24ecffac70e7c8a41894b140b87fda
Nil Adell Mill <nila@ethz.ch>
2020-11-02 16:38:01 +0100

    feat: Add __main__ to reactions vae

--- paccmann_polymer/topologically_regularized_models/experiments/chem_reactions/reactions_vae.py
+++ paccmann_polymer/topologically_regularized_models/experiments/chem_reactions/reactions_vae.py
@@ -119,3 +119,154 @@ def train(
         f"\tkl_scale {kl_scale}"
     )
     logger.info(f'{epoch}\t{train_loss/_iter}\t{time()-start_time}')
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Graph reactions example')
+
+    parser.add_argument(
+        '--epochs',
+        type=int,
+        default=15,
+        metavar='N',
+        help='number of epochs to train (default: 15)'
+    )
+    parser.add_argument(
+        '--gr', type=float, required=True, help='Graph regularizer value'
+    )
+    parser.add_argument(
+        '--lr',
+        type=float,
+        default=1e-4,
+        help='Learning rate (default 1e-4)'
+    )
+    parser.add_argument('-v', action='store_true', help='Verbose (tqdm)')
+
+    args = parser.parse_args()
+
+    logger.info('Running model')
+
+    graph_gamma = args.gr
+    epochs = args.epochs
+    verbose = args.v
+    lr = args.lr
+
+    # FILE DEFS
+    DATA_FOLDER =  # Folder with the processed reactions dataset
+    PROCESSED_FILE = os.path.join(
+        DATA_FOLDER, 'MIT_mixed', 'processed-train.json'
+    )  # Processed reactions dataset file
+
+    # For more info see https://github.com/PaccMann/paccmann_chemistry
+    MDL_DIR =  # Model directory
+    PARAM_FILE =  # Model params
+    WEIGHT_FILE =  # Model weights
+    LANG_FILE =  # Language file
+
+    device = get_device()
+
+    model_dir = os.path.join(
+        './models_react/20k_data/topo_paccmann',
+        f'reg_strength_{graph_gamma}-lr_{lr}'
+    )
+
+    writer = SummaryWriter(f'logs/20k_data/react_gcvae_{graph_gamma}/lr_{lr}')
+    save_dir = os.path.join(model_dir, f'weights')
+    os.makedirs(save_dir, exist_ok=True)
+
+    paccmann_vae = Encoder(
+        PARAM_FILE, LANG_FILE, WEIGHT_FILE, batch_size=1, batch_mode='Packed'
+    )
+    paccmann_vae.gru_vae.to(device)
+    paccmann_vae.gru_vae.encoder.set_batch_mode('packed')
+
+    optimizer = OPTIMIZER_FACTORY['adam'](
+        paccmann_vae.gru_vae.parameters(), lr=lr
+    )
+
+    # scheduler = optim.lr_scheduler.ReduceLROnPlateau(
+    #     optimizer, verbose=True, threshold=1E-2, eps=1e-6
+    # )
+    scheduler = optim.lr_scheduler.StepLR(
+        optimizer, 12, gamma=0.1, last_epoch=-1
+    )
+
+    loader = load_data(PROCESSED_FILE, LANG_FILE)
+
+    # Should load intermediate too, but for now let's just grab those
+    # where we actually finished the training.
+    last_model = os.path.join(save_dir, f'saved_model_epoch_last.pt') #{epochs}.pt')
+    if os.path.exists(last_model):
+        paccmann_vae.update_vae_weights(last_model)
+    else:
+        for epoch in range(epochs):
+            train(
+                epoch,
+                paccmann_vae,
+                loader,
+                optimizer,
+                scheduler,
+                graph_gamma,
+                writer=writer,
+                verbose=verbose
+            )
+            paccmann_vae.gru_vae.save(
+                os.path.join(save_dir, f'saved_model_epoch_{epoch+1}.pt')
+            )
+        paccmann_vae.gru_vae.save(
+            os.path.join(save_dir, 'saved_model_epoch_last.pt')
+        )
+
+    # In the future we should merge the downstream task from the different
+    # models into a single formula
+    model = paccmann_vae
+
+    # Processed downstram taks
+    DOWNSTREAM_TRAIN_FILE = os.path.join(
+        DATA_FOLDER, 'MIT_mixed', 'downstream_train.json'
+    )
+    DOWNSTREAM_TEST_FILE = os.path.join(
+        DATA_FOLDER, 'MIT_mixed', 'downstream_test.json'
+    )
+
+    # Downstream task
+    dataset = CSVDataset(DOWNSTREAM_TRAIN_FILE, LANG_FILE)
+    train_latent_data = []
+    train_target = []
+    model.update_batch_size(2)
+    for seqs, y in dataset:
+        encoder_seq, _, _ = packed_sequential_data_preparation(
+            seqs, input_keep=1., start_index=2, end_index=3, device=device
+        )
+
+        z = model.encode(encoder_seq)
+        train_latent_data.append(
+            z.view(1, -1).squeeze().cpu().detach().numpy()
+        )
+        train_target.append(y)
+    train_latent_data = np.stack(train_latent_data)
+    train_target = np.array(train_target)
+
+    # Downstream test
+    dataset = CSVDataset(DOWNSTREAM_TEST_FILE, LANG_FILE)
+    test_latent_data = []
+    test_target = []
+    model.update_batch_size(2)
+    for seqs, y in dataset:
+        encoder_seq, _, _ = packed_sequential_data_preparation(
+            seqs, input_keep=1., start_index=2, end_index=3, device=device
+        )
+
+        z = model.encode(encoder_seq)
+        test_latent_data.append(z.view(1, -1).squeeze().cpu().detach().numpy())
+        test_target.append(y)
+    test_latent_data = np.stack(test_latent_data)
+    test_target = np.array(test_target)
+
+    acc = compute_logistic_regression_accuracy(
+        train_latent_data, train_target, test_latent_data, test_target
+    )
+    writer.add_scalar('accuracy', acc)
+    logger.info(f'Downstream accuracy: {acc}')
+    logger.info('Done downstream!')
