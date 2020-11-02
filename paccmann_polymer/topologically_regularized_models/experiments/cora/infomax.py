598d9f89207daab3501d6948ce8f6aea4198de4e
Nil Adell Mill <nila@ethz.ch>
2020-11-02 16:50:00 +0100

    feat: Add __main__ to cora infomax

--- paccmann_polymer/topologically_regularized_models/experiments/cora/infomax.py
+++ paccmann_polymer/topologically_regularized_models/experiments/cora/infomax.py
@@ -29,23 +29,58 @@ class Encoder(nn.Module):
 def corruption(x, edge_index):
     return x[torch.randperm(x.size(0))], edge_index
 
-def train():
-    model.train()
-    optimizer.zero_grad()
-    pos_z, neg_z, summary = model(data.x, data.edge_index)
-    loss = model.loss(pos_z, neg_z, summary)
-    loss.backward()
-    optimizer.step()
-    return loss.item()
 
-def test():
-    model.eval()
-    z, _, _ = model(data.x, data.edge_index)
-    acc = model.test(
-        z[data.train_mask],
-        data.y[data.train_mask],
-        z[data.test_mask],
-        data.y[data.test_mask],
-        max_iter=150
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Graph synthetic example')
+    parser.add_argument(
+        'dataset',
+        type=str,
+        choices=['Cora', 'CiteSeer', 'PubMed'],
+        help='Which dataset to use'
     )
-    return acc
+
+    args = parser.parse_args()
+    dataset = args.dataset  # 'CiteSeer'  # 'Cora'
+    writer = SummaryWriter(f'logs_{dataset.lower()}/infomax')
+    path = osp.join('.', 'data', dataset)
+    dataset = Planetoid(path, dataset)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = DeepGraphInfomax(
+        hidden_channels=512,
+        encoder=Encoder(dataset.num_features, 512),
+        summary=lambda z, *args, **kwargs: torch.sigmoid(z.mean(dim=0)),
+        corruption=corruption
+    ).to(device)
+    data = dataset[0].to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+    def train():
+        model.train()
+        optimizer.zero_grad()
+        pos_z, neg_z, summary = model(data.x, data.edge_index)
+        loss = model.loss(pos_z, neg_z, summary)
+        loss.backward()
+        optimizer.step()
+        return loss.item()
+
+    def test():
+        model.eval()
+        z, _, _ = model(data.x, data.edge_index)
+        acc = model.test(
+            z[data.train_mask],
+            data.y[data.train_mask],
+            z[data.test_mask],
+            data.y[data.test_mask],
+            max_iter=150
+        )
+        return acc
+
+    for epoch in range(1, 301):
+        loss = train()
+        writer.add_scalar('loss', loss, epoch)
+        print('Epoch: {:03d}, Loss: {:.4f}'.format(epoch, loss))
+    acc = test()
+    writer.add_scalar('accuracy', acc)
+    print('Accuracy: {:.4f}'.format(acc))
